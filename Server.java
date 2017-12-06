a2e9a612510a6cb9a11f2f15c02425d2d863cf27
Abdul Rehman <abudrehman45@gmail.com>
2017-12-06 12:53:40 -0600

    server stuff

--- Server.java
+++ Server.java
@@ -1,64 +1,54 @@
-import java.net.*; 
+import java.net.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 import java.io.*; 
 
 public class Server 
 { 
- public static void main(String[] args) throws IOException 
-   { 
-    ServerSocket serverSocket = null; 
-
-    try { 
-         serverSocket = new ServerSocket(10007); 
-        } 
-    catch (IOException e) 
-        { 
-         System.err.println("Could not listen on port: 10007."); 
-         System.exit(1); 
-        } 
-
-    Socket clientSocket = null; 
-
-    try { 
-         System.out.println ("Waiting for Client");
-         clientSocket = serverSocket.accept(); 
-        } 
-    catch (IOException e) 
-        { 
-         System.err.println("Accept failed."); 
-         System.exit(1); 
-        } 
-
-//    ObjectOutputStream out = new ObjectOutputStream(
-//                                     clientSocket.getOutputStream()); 
-//    ObjectInputStream in = new ObjectInputStream( 
-//                                     clientSocket.getInputStream()); 
-    
-    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-    PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
-
-    String pt3 = null;
-    String pt4 = null;
-    do{
-    try {
-         pt3 = (String) in.readLine();
-        }
-    catch (Exception ex)
-        {
-         System.out.println (ex.getMessage());
-        }
-
-
-    System.out.println ("Server recieved point: " + pt3 + " from Client");
-
-    pt4 = "Server Received '" + pt3 +"'" ;
-    System.out.println ("Server sending point: " + pt4 + " to Client");
-    out.println(pt4); 
-    out.flush();
-    }while (!pt3.equals("DIE"));
-
-    out.close(); 
-    in.close(); 
-    clientSocket.close(); 
-    serverSocket.close(); 
-   } 
+	private ServerSocket central;
+	private Socket client;
+	private ObjectOutputStream out;
+	private ObjectInputStream in;
+	private ArrayList<Integer> Test;
+	
+	private HashMap <String, Socket> cList;
+	
+	public Server() {
+		cList = new HashMap<String, Socket>();
+		try {
+			this.central = new ServerSocket(4454);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		System.out.println("WAITING");
+		while(true){
+			this.acceptConnect();
+			
+		}
+		
+	}
+	
+	
+	private void acceptConnect(){
+		try {
+			this.client = this.central.accept();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		this.cList.put(this.client.getInetAddress().getHostName(), this.client);
+		
+		new CommThread(this.client).start();
+	}
+	
+	
+	public static void main(String[] args){
+		Server S = new Server();
+	}
+	
 } 
+
